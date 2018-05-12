6d0e6d7d408e3544958c2a133a212229a03764bc
jinsphan <pvtinh1996@gmail.com>
2018-05-12 23:27:59 +0700

    Upadte

--- controllers/quiz_controller.php
+++ controllers/quiz_controller.php
@@ -7,6 +7,52 @@
         public function add() {
             $this->display();
         }
+
+        public function start($id_quiz) {
+            $this->data_quiz = [
+                [
+                    "name_question" => "Lựa ch đúng nhất:",
+                    "correct_answer" => [1],
+                    "answers" => [
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên a lớn nhất biết 225 ⋮ a và 160 ⋮ a. Vậy:",
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên x lớn nhất biết 35 ⋮ x và 49 ⋮ x."
+                    ],
+                ],
+                [
+                    "name_question" => "Lựa chọn đáp án đúng nhất:",
+                    "correct_answer" => [1],
+                    "answers" => [
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên a lớn nhất biết 225 ⋮ a và 160 ⋮ a. Vậy:",
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên x lớn nhất biết 35 ⋮ x và 49 ⋮ x."
+                    ],
+                ],
+                [
+                    "name_question" => "Lựa chọn đáp án đúng nhất:",
+                    "correct_answer" => [1],
+                    "answers" => [
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên a lớn nhất biết 225 ⋮ a và 160 ⋮ a. Vậy:",
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên x lớn nhất biết 35 ⋮ x và 49 ⋮ x."
+                    ],
+                ],
+                [
+                    "name_question" => "Lựa chọn đáp án đúng nhất:",
+                    "correct_answer" => [1],
+                    "answers" => [
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên a lớn nhất biết 225 ⋮ a và 160 ⋮ a. Vậy:",
+                        "Toi rat la dau khi nghe bai cua son Tung",
+                        "Tìm số tự nhiên x lớn nhất biết 35 ⋮ x và 49 ⋮ x."
+                    ],
+                ]
+            ];
+            $this->display();
+        }
         
     }
 ?>
---
+++ views/quiz/start.php
@@ -0,0 +1,47 @@
+<?php include_once 'views/layout/'.$this->layout.'header.php'; ?>
+
+<link href="<?php echo RootREL; ?>media/css/start.css" rel="stylesheet">
+
+<div class="start-container">
+    <div class="container">
+        <div class="start-name-quiz text-center">
+            <h3>Toán lớp 1 - Đề kiểm tra 15 phút - Tháng 8 Miễn phí</h3>
+        </div>
+        <div class="start-content box-shadow">
+            <div class="time-countdown">
+                <div class="title bg-main">Thời gian còn lai</div>
+                <div class="time">
+                    02:59
+                </div>
+            </div>
+            <?php foreach($this->data_quiz as $index => $question) { ?>
+            <div class="item-question">
+                <div class="tag-num-question">
+                    <div class="name-number">
+                        <span>Câu <?= $index+1 ?>:</span>
+                    </div>
+                    <div class="score-question">
+                        <span>(10 diem)</span>
+                    </div>
+                </div>
+                <div class="question-content">
+                    <p class="p"><?= $question["name_question"] ?></p>
+                    <div class="anserwers-content">
+                        <?php foreach($question["answers"] as $index_as => $answer) { ?>
+                        <div>
+                            <input name="<?= $index ?>" type="radio" id="<?= $index . "-". $index_as ?>"> <label for="<?= $index . "-". $index_as ?>"><?= $answer ?></label>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+
+            <div class="btn-submit">
+                <button class="btn-v2">Nộp bài</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'views/layout/'.$this->layout.'footer.php'; ?>
